8a16c8627857e0c1094dca1518ec9543345eae6f
Mininh <danielmarchenajimenez2002@gmail.com>
2023-07-20 22:35:43 +0200

    Carousel funcionality and ability box

---
+++ src/data/abilities.js
@@ -0,0 +1,42 @@
+export const abilities = [
+    {
+        id: 'astro',
+        title: 'Astro',
+        abbreviation: 'Astro',
+        icon: './abilities-icons/astro.svg',
+        color: 'midnightblue',
+        bgColor: 'midnightblue'
+    },
+    {
+        id: 'css',
+        title: 'CSS',
+        abbreviation: 'CSS',
+        icon: './abilities-icons/css3.svg',
+        color: 'royalblue',
+        bgColor: 'royalblue'
+    },
+    {
+        id: 'html',
+        title: 'HTML',
+        abbreviation: 'HTML',
+        icon: './abilities-icons/html5.svg',
+        color: 'orange',
+        bgColor: 'orange'
+    },
+    {
+        id: 'js',
+        title: 'JavaScript',
+        abbreviation: 'JS',
+        icon: './abilities-icons/javascript.svg',
+        color: 'yellow',
+        bgColor: 'yellow'
+    },
+    {
+        id: 'tailwind',
+        title: 'Tailwind',
+        abbreviation: 'Tailwind',
+        icon: './abilities-icons/tailwindcss.svg',
+        color: 'skyblue',
+        bgColor: 'skyblue'
+    },
+]
